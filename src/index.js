b3430c82800b2669b88ca2fc12590dda91012a8f
Vladislav <>
2019-09-29 10:37:16 +0300

    feat: add task 85%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let openBrackets = [];
+    let closeBrackets = [];
+    let mapBrackets = {};
+    let arr = str.split("");
+    let stack = [];
+    let last = "";
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        openBrackets.push(bracketsConfig[i][0]);
+        closeBrackets.push(bracketsConfig[i][1]);
+        mapBrackets[openBrackets[i]] = closeBrackets[i];
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (openBrackets.includes(arr[i]) && closeBrackets.includes(arr[i]))
+            continue;
+        if (openBrackets.includes(arr[i])) {
+            stack.push(arr[i]);
+        } else {
+            last = stack.pop();
+            if (arr[i] !== mapBrackets[last]) return false;
+        }
+    }
+    if (stack.length !== 0) return false;
+    return true;
+};
